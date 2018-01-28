4e1caa259da4a93cf8af27bfd574491da8d160cb
tom <tntdavison@gmail.com>
2018-01-28 17:23:35 -0600

    Function that generates name in formatted fashion

---
+++ name_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+def get_formatted_name(first, last):
+    """Function to generate well formatted
+    first and last name."""
+    full_name = first + ' '+ last
+    return full_name.title()
+
+get_formatted_name("tommy", "davison")
