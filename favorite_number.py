bb6210acd6ab2c097ecd8ebd5aaa8d70c9831389
tom <tntdavison@gmail.com>
2018-01-28 17:15:15 -0600

    New module that takes a users favorite number and stores into a file. Does some insanity checks to get number if exists

--- favorite_number.py
+++ favorite_number.py
@@ -2,47 +2,34 @@
 import json
 
 
-def data_info():
-    """Function that will gather users name
-    and favorite number. Function will save
-    user info as a dictionary and save to a
-    JSON file for later reference."""
-
-    data = {}
-
-    data['name'] = input("Enter your name: ")
-    data['number'] = input("Enter your favorite number: ")
-
-    return data
-
-
-def save_info(filename):
-    """Function that will save user info to a file"""
-    info = user_info()
-
-    with open(filename, 'w') as f_obj:
-        json.dump(info, f_obj)
-    f_obj.close()
-
-
-def check_existance(filename):
-    """Function that will check file
-    to see if user exists.
-
-    exists = True
+def get_stored_number():
+    """Function to get stored number if available"""
+    filename = "favorite_number.json"
+    try:
+        with open(filename, 'r') as f_obj:
+            number = json.load(f_obj)
+    except FileNotFoundError:
+        return None
     else:
-        False"""
-    #user_data = user_info()
+        return number
 
 
-    with open(filename, 'r') as f_obj:
-        info = json.load(f_obj)
-    f_obj.close()
+def get_new_number():
+    """Function to store favorite number"""
+    filename = "favorite_number.json"
+    number = input("Please enter your favorite number: ")
+    with open(filename, 'w') as f_obj:
+        json.dump(number, f_obj)
+        f_obj.close()
 
-    if user_data['name'] in info:
-        print("User already exists.\n")
-        print("Here is the info we have stored: ")
-        print(user_data)
+def present_number():
+    """Function to print favorite number"""
+    number = get_stored_number()
 
-save_info("userinfo.json")
-check_existance("userinfo.json")
+    if number:
+        print("I know your favorite number: ", number)
+    else:
+        create_number = get_new_number()
+        print("I know your favorite number: ", create_number)
+
+present_number()
