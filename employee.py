399c64c978e479497e4f101af6095c70802ed052
tom <tntdavison@gmail.com>
2018-01-31 05:59:34 -0600

    Made logic changes to the give_raise method

--- employee.py
+++ employee.py
@@ -4,16 +4,16 @@ class Employee():
     """Gathers employee info"""
 
 
-    def __init__(self, first, last, salary='$5,000'):
+    def __init__(self, fullname, salary='$5,000'):
         """Init all needed args for this class module"""
-        self.first = first
-        self.last = last
+        self.fullname = fullname
         self.salary = salary
+
 
     def give_raise(self, new_salary=''):
         """Give employee a raise
         this can also be a custom amount"""
-        if new_salary == False:
-            return self.salary
+        if new_salary:
+            print(new_salary)
         else:
-            return new_salary
+            print(self.salary)
